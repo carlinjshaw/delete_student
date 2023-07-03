d320732266b5faa2e1b5c7619c6cb76a0d73af2d
OliveraDiaz <130876122+OliveraDiaz@users.noreply.github.com>
2023-07-03 16:14:02 -0500

    added CSS

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
 
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -15,12 +16,7 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    
-    var currentHour = dayjs().hour()
-    
-    if currentHour === currentHour{
-      
-    }
+ 
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
@@ -32,3 +28,4 @@ $(function () {
       // TODO: Add code to display the current date in the header of the page
 var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
+
